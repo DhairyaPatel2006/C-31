3b0812dfdc4111c883f3710bc90846103841bb07
DhairyaPatel2006 <dhairyappatel23@gmail.com>
2020-12-09 18:01:22 +0530

    helo

--- Bird.js
+++ Bird.js
@@ -2,12 +2,26 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.image1 = loadImage("sprites/smoke.png");
+
+    this.arr = [];
   }
 
   display() {
-    //this.body.position.x = mouseX;
-    //this.body.position.y = mouseY;
-
+  
     super.display();
+  
+    if(this.body.velocity.x > 3 && this.body.position.x > 200){
+
+      var dat = [this.body.position.x,this.body.position.y];
+      this.arr.push(dat);
+
+    }
+    if(gameState === "launched"){
+    for (var tr = 0; tr < this.arr.length; tr++){
+      image(this.image1,this.arr[tr][0],this.arr[tr][1]);
+    }
+  }
+
   }
 }
--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+var gameState = "onsling";
 
 
 function preload() {
@@ -69,16 +70,22 @@ function draw(){
 }
 
 function mouseDragged(){
+    if (gameState === "onsling") {
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "launched"
 }
 
 function keyPressed(){
-    if(keyCode === 32){
+
+
+    if(keyCode === 32 && gameState === "launched"){
         slingshot.attach(bird.body);
+        gameState = "onsling"
     }
 }
